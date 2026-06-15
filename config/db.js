199023dde1399dcd064f9f2e4a1a80922c44c18f
trankhai01 <trankhai132905@example.com>
2026-06-15 15:58:02 +0700

    Support DB_PORT environment variable

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'quan_ly_gym',
